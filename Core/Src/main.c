b10a5ff928544efb7f303fe76434037071422381
Niel Cansino <nielcansino@gmail.com>
2021-07-31 19:33:27 +0900

    current WIP with segger uart

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -96,8 +96,10 @@ int main(void)
   /* Enable cycle counting by setting the bit 0 of DWT_CTRL */
   *DWT_CTRL |= 0x1;
 
+  SEGGER_UART_init(500000);
+
   SEGGER_SYSVIEW_Conf();
-  SEGGER_SYSVIEW_Start();
+  // SEGGER_SYSVIEW_Start();	// no need to do this since SEGGER_SYSVIEW_Start() is already clled inside SEGGER_UART_init();
 
   /* Create the task, storing the handle. */
   task1_rv = xTaskCreate(vTask1, "Task 1", 200, "x111111111111111111111111111111111x", 1, &task1_handle);
@@ -147,8 +149,8 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 50;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -163,7 +165,7 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
   {
     Error_Handler();
   }
@@ -311,7 +313,7 @@ static void MX_GPIO_Init(void)
 
 static void vTask1(void * pvParameters)
 {
-	char msg[100];
+	// char msg[100];
 
 	while(1)
 	{
@@ -327,7 +329,7 @@ static void vTask1(void * pvParameters)
 
 static void vTask2(void * pvParameters)
 {
-	char msg[100];
+	// char msg[100];
 
 	while(1)
 	{
